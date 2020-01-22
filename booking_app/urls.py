f27e80099e4bfab158c4298ca32f601e9848f06c
nastinsk <nastinsk@gmail.com>
2020-01-22 01:41:14 -0800

    Postgresql db added

--- booking_app/urls.py
+++ booking_app/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from .views import ReservationsList, ReservationDetail
 
 urlpatterns = [
+  
+  # path('<int:pk>/', ReservationDetail.as_view()),
+  # path('', ReservationsList.as_view()),
+
   path('reservations/', ReservationsList.as_view(), name='reservations_list'),
   path('reservations/<int:pk>/', ReservationDetail.as_view(), name='reservation_detail')
 ]
